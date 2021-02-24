224956d01be8df88fa689340a58d58e27e661492
Vadim Khizhnyakov <provcydia@gmail.com>
2021-02-25 02:35:31 +0300

    starting block added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
+const port = process.env.PORT || '8000';
 const app = express();
 app.use('/', express.static(__dirname));
-app.listen(process.env.PORT || '8000', () => {
-  console.log('Server strted on http://127.0.0.1:8000');
+app.listen(port, () => {
+  console.log(`Server strted on http://127.0.0.1:${port}`);
 });
