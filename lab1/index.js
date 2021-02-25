f76fa698a84ee3b360e3bda9b972fad2373a1163
Vadim Khizhnyakov <provcydia@gmail.com>
2021-02-26 02:37:34 +0300

    Added foreword, pluses and minuses, navigation

--- lab1/index.js
+++ lab1/index.js
@@ -1,1 +1,5 @@
-console.log('darova');
+window.onload = () => {
+  const starting = document.getElementById('start-block');
+  const navHeight = document.getElementById('nav-block').offsetHeight;
+  starting.style.height = `${(starting.offsetHeight - navHeight)}px`;
+};
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || '8000';
 const app = express();
 app.use('/', express.static(__dirname));
 app.listen(port, () => {
-  console.log(`Server strted on http://127.0.0.1:${port}`);
+  console.log(`Server started on http://127.0.0.1:${port}`);
 });
