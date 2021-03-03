87b361df1d62977d2b4dbf2a1a1690e0028a0ec8
Vadim Khizhnyakov <provcydia@gmail.com>
2021-03-04 02:58:50 +0300

    minor fixes, if you know what im talkng about

--- lab2/index.js
+++ lab2/index.js
@@ -1,5 +1,6 @@
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let isVisible = vw >= 786;
+const loginBtnVWMarker = 400;
 const navToggler = document.querySelector('button.navbar-toggler');
 
 navToggler.addEventListener('click', (navToggled) => {
@@ -15,12 +16,12 @@ navToggler.addEventListener('click', (navToggled) => {
     visibleNavLinksContainer.classList.toggle('visible-nav-links-container');
     visibleNavLinksContainer.classList.toggle('hidden');
     visibleNavLinksContainer.appendChild(navLinks);
-    if (vw <= 320) {
+    if (vw <= loginBtnVWMarker) {
       loginBtn.classList.toggle('login-btn-visible');
       visibleNavLinksContainer.appendChild(loginBtn);
     }
     // eslint-disable-next-line no-nested-ternary
-    const additionalPadding = vw <= 320 ? '280px' : (vw >= 440 && vw <= 768 ? '140px' : '200px');
+    const additionalPadding = vw <= loginBtnVWMarker ? '280px' : (vw >= 440 && vw <= 768 ? '140px' : '200px');
     document.querySelector('h1.start-h').style.paddingTop = additionalPadding;
     document.querySelector('body').appendChild(visibleNavLinksContainer);
     setTimeout(() => {
@@ -36,7 +37,7 @@ navToggler.addEventListener('click', (navToggled) => {
       });
       const linksNavItem = document.querySelectorAll('.nav-item')[1];
       linksNavItem.appendChild(visibleNavLinksContainer.firstChild);
-      if (vw <= 320) {
+      if (vw <= loginBtnVWMarker) {
         const loginBtnNavItem = document.querySelectorAll('.nav-item')[2];
         loginBtn.classList.toggle('login-btn-visible');
         loginBtnNavItem.appendChild(loginBtn);
