ffb5e0f4e3e19bfd0b3ab64a1b460254b66f8a34
Nikita Bakhramov <nikikostick@gmail.com>
2021-04-25 20:39:57 +0300

    may be this will fix vercel

--- lab9/src/index.js
+++ lab9/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter, BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
+const app= (
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-    <App />
-    </Router>
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
